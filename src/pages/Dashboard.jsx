642aca66c1431730ad6cd597d1f53f75088dbfe4
mohamed <mohamedabid204@gmail.com>
2025-01-03 19:41:36 +0200

    add

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import Statistics from "../components/Statistics";
 import styles from "../assets/styles/Dashboard.module.css";
 const Dashboard = () => {
   return (
+    
     <div className={styles.container}>
       <h1>Employee Dashboard</h1>
       <Statistics />
